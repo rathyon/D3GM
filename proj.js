b66fae6760bbb1e5ce46c6811276eda8044f5dc2
Francisco Dourado <francisco.dourado@hotmail.com>
2018-12-10 17:04:31 +0000

    radarchart tweak

--- proj.js
+++ proj.js
@@ -857,20 +857,10 @@ function gen_treemap(){
   }
 }
 
-function init(){
-	
-}
 
 function init_radarchart(){
-	var w = 200;
-	var h = 200;
-/*
-	d3.csv("games.csv", function (data){
-		games = data;
-		return 0;
-		//gen_heatmap();
-	});
-*/
+	var w = 300;
+	var h = 300;
 
 	//Legend titles
 	var LegendOptions = ['Games'];
@@ -924,8 +914,8 @@ function init_radarchart(){
 		};
 	//Call function to draw the Radar chart
 	//Will expect that data is in %'s
-	gen_radarchart("#radarchart1", movies_radar);
-	gen_radarchart("#radarchart2", games_radar, cfg2);
+	gen_radarchart("#radarchart1", games_radar);
+	gen_radarchart("#radarchart2", movies_radar, cfg2);
 
 	////////////////////////////////////////////
 	/////////// Initiate legend ////////////////
@@ -942,40 +932,12 @@ function init_radarchart(){
 		.attr("class", "title")
 		.attr('transform', 'translate(20,0)') 
 		.attr("x", w - 100)
-		.attr("y", 10)
-		.attr("font-size", "12px")
-		.attr("fill", "#404040")
+		.attr("y", 20)
+		.attr("font-size", "14px")
+		.attr("fill", "white")
 		.text("Number of Games sold(M)");
 			
-	//Initiate Legend	
-	var legend = svg.append("g")
-		.attr("class", "legend")
-		.attr("height", 100)
-		.attr("width", 200)
-		.attr('transform', 'translate(90,20)') 
-		;
-		//Create colour squares
-		legend.selectAll('rect')
-		  .data(LegendOptions)
-		  .enter()
-		  .append("rect")
-		  .attr("x", w - 65)
-		  .attr("y", function(d, i){ return i * 20;})
-		  .attr("width", 10)
-		  .attr("height", 10)
-		  .style("fill", "blue")//function(d, i){ return colorscale(3);})
-		  ;
-		//Create text next to squares
-		legend.selectAll('text')
-		  .data(LegendOptions)
-		  .enter()
-		  .append("text")
-		  .attr("x", w - 52)
-		  .attr("y", function(d, i){ return i * 20 + 9;})
-		  .attr("font-size", "11px")
-		  .attr("fill", "#737373")
-		  .text(function(d) { return d; })
-		  ;	
+	
 		  
 	var svg2 = d3.select('#radarchart2')
 		.selectAll('svg')
@@ -988,47 +950,19 @@ function init_radarchart(){
 		.attr("class", "title")
 		.attr('transform', 'translate(20,0)') 
 		.attr("x", w - 100)
-		.attr("y", 10)
-		.attr("font-size", "12px")
-		.attr("fill", "#404040")
+		.attr("y", 20)
+		.attr("font-size", "14px")
+		.attr("fill", "white")
 		.text("Number of Movies sold(M)");
 			
-	//Initiate Legend	
-	var legend = svg2.append("g")
-		.attr("class", "legend")
-		.attr("height", 100)
-		.attr("width", 200)
-		.attr('transform', 'translate(90,20)') 
-		;
-		//Create colour squares
-		legend.selectAll('rect')
-		  .data(LegendOptions2)
-		  .enter()
-		  .append("rect")
-		  .attr("x", w - 65)
-		  .attr("y", function(d, i){ return i * 20;})
-		  .attr("width", 10)
-		  .attr("height", 10)
-		  .style("fill", "green")//function(d, i){ return colorscale(3);})
-		  ;
-		//Create text next to squares
-		legend.selectAll('text')
-		  .data(LegendOptions2)
-		  .enter()
-		  .append("text")
-		  .attr("x", w - 52)
-		  .attr("y", function(d, i){ return i * 20 + 9;})
-		  .attr("font-size", "11px")
-		  .attr("fill", "#737373")
-		  .text(function(d) { return d; })
-		  ;	
+	
 }
 
 function gen_radarchart(id, d, options){
   var cfg = {
 	 radius: 5,
-	 w: 200,
-	 h: 200,
+	 w: 300,
+	 h: 300,
 	 factor: 1,
 	 factorLegend: .85,
 	 levels: 3,
@@ -1036,10 +970,10 @@ function gen_radarchart(id, d, options){
 	 radians: 2 * Math.PI,
 	 opacityArea: 0.5,
 	 ToRight: 5,
-	 TranslateX: 90,
+	 TranslateX: 140,
 	 TranslateY: 80,
-	 ExtraWidthX: 200,
-	 ExtraWidthY: 200,
+	 ExtraWidthX: 280,
+	 ExtraWidthY: 120,
 	 color: "blue"
 	};
 	
